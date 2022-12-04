81383833d59aeb0bf4bad50197ed20f05479ef56
MohammadParvez <mohammadparvez00966@gmail.com>
2022-12-05 01:07:57 +0600

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const jwt = require("jsonwebtoken");
+const {
+  MongoClient,
+  ServerApiVersion,
+  ObjectId,
+  ObjectID,
+} = require("mongodb");
 require("dotenv").config();
 
 const app = express();
@@ -16,11 +22,89 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+async function run() {
+  try {
+    const serviceCollection = client.db("sdpProject").collection("services");
+    const donationCollection = client.db("sdpProject").collection("donations");
+
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
+
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
+    // donations api
+    app.get("/donations", async (req, res) => {
+      let query = {};
+      if (req.query.email) {
+        query = {
+          email: req.query.email,
+        };
+      }
+      const cursor = donationCollection.find(query);
+      const donations = await cursor.toArray();
+      res.send(donations);
+    });
+
+    app.post("/donations", async (req, res) => {
+      const donation = req.body;
+      const result = await donationCollection.insertOne(donation);
+      res.send(result);
+    });
+
+    app.patch("/donations/:id", async (req, res) => {
+      const id = req.params.id;
+      const status = req.body.status;
+      const query = { _id: ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          status: status,
+        },
+      };
+      const result = await donationCollection.updateOne(query, updatedDoc);
+      res.send(result);
+    });
+
+    // app.delete("/donations/:id", async (req, res) => {
+    //   const id = req.params.id;
+    //   const query = { _id: ObjectId(id) };
+    //   const result = await donationCollection.deleteOne(query);
+    //   res.send(result);
+    // });
+    // app.put("/donations/admin/:id", async (req, res) => {
+    //   const id = req.params.id;
+    //   const filter = { _id: ObjectId() };
+    //   const options = { upsert: true };
+    //   const updatedDoc = {
+    //     $set: {
+    //       role: "admin",
+    //     },
+    //   };
+    //   const result = await donationCollection.updateOne(
+    //     filter,
+    //     updatedDoc,
+    //     options
+    //   );
+    //   res.send(result);
+    // });
+  } finally {
+  }
+}
+run().catch((err) => console.error(err));
 
 app.get("/", (req, res) => {
   res.send("genius car server is running");
